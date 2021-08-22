6bff587ac55171fad34f0e02a0554edb5f82dc41
Lautaro Fernandez <88322346+lautarox16@users.noreply.github.com>
2021-08-22 18:13:03 -0300

    Entrega  1
    Entrega 1, se modifica products.js
    se agrega un script en index.html
    se crea login.css
    se modifica login.js
    se modifica login.html

---
+++ eCommerce--main/js/login.js
@@ -0,0 +1,19 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+const login = () => {
+  // verifico que el usuario y contraseña no esten vacios
+  if (
+    document.getElementById("usuario").value &&
+    document.getElementById("contraseña").value
+  ) {
+    sessionStorage.setItem("logueado", true);
+    window.location = "index.html";
+  } else {
+    alert("Usuario y contraseña no deben ser vacíos");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  document.getElementById("log").addEventListener("click", login);
+});
